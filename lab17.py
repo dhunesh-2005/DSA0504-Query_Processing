97fbcb7db1dd596cd90c0c4be943c15a76552200
Dhuneshwaran <thinkgood24hr@gmail.com>
2025-12-25 14:51:03 +0530

    Add DataFrame creation and aggregation for ages

---
+++ lab17.py
@@ -0,0 +1,18 @@
+#17
+import pandas as pd
+
+data = {
+    'school_code': ['s001', 's002', 's003', 's001', 's002', 's004'],
+    'class': ['V', 'V', 'VI', 'VI', 'V', 'VI'],
+    'name': ['Alberto Franco', 'Gino Mcneill', 'Ryan Parkes',
+             'Eesha Hinton', 'Gino Mcneill', 'David Parkes'],
+    'age': [12, 12, 13, 13, 14, 12]
+}
+
+df = pd.DataFrame(data)
+#print(df)
+#
+# Group by school code and calculate mean, min, max of age
+result = df.groupby('school_code')['age'].agg(['mean', 'min', 'max'])
+
+print(result)
